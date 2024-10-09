c52bb84b10036f80c454b655487374b78aa60fd5
hdh4952 <gkseogml4959@gmail.com>
2024-10-09 04:53:59 +0000

    style: PostCard 디자인 적용

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,28 @@
-export default function PostCard({tag, title, description, date}: {tag: string, title: string, description: string, date: string}) {
-    return <div className='border border-red-400 w-11/12 lg:w-[800px] h-40 mb-8'>
-        <div>tag: {tag}</div>
-        <div>title: {title}</div>
-        <div>description: {description}</div>
-        <div>date: {date}</div>
+import Link from 'next/link';
+
+export default function PostCard({
+  tag,
+  title,
+  description,
+  date,
+}: {
+  tag: string;
+  title: string;
+  description: string;
+  date: string;
+}) {
+  return (
+    <div className="m-8 h-40 w-11/12 rounded-2xl border shadow lg:w-[800px]">
+      <div className="flex h-16 w-full p-4">
+        <Link href={`/${tag}`}>
+          <div className="flex h-8 w-32 items-center justify-center rounded-lg bg-black3 text-sm font-bold text-white">
+            {tag}
+          </div>
+        </Link>
+        <div className="flex h-8 w-full items-center justify-start pl-4 text-lg font-bold">{title}</div>
+      </div>
+      <div className="h-16 px-4 text-base font-normal">description: {description}</div>
+      <div className="px-4 text-sm font-normal">{date}</div>
     </div>
+  );
 }
