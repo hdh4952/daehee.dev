95ecc08c3ea5326dfeaf3304c678e6eaf9ef7231
hdh4952 <gkseogml4959@gmail.com>
2024-10-10 14:16:43 +0000

    feat: Footer 생성

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex h-20 w-full justify-center bg-white">
+      <div className="flex w-full items-center justify-center px-4 lg:w-[1024px]">
+        <span>© 2024. Daehee Han all rights reserved.</span>
+      </div>
+    </footer>
+  );
+}
