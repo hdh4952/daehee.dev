cdaa88ac91c053bded3d401e974a5b6cda2ae276
hdh4952 <gkseogml4959@gmail.com>
2024-10-10 14:13:59 +0000

    feat: Header Link 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import { MdDarkMode } from 'react-icons/md';
 
 export default function Header() {
   return (
-    <header className="border-b-gray3 flex h-16 w-full justify-center border-b">
-      <div className="flex w-full items-center justify-between lg:w-[1024px]">
-        <span className="p-1 text-xl font-bold text-black">daehee.dev</span>
+    <header className="fixed top-0 flex h-16 w-full justify-center border-b border-b-gray3 bg-white">
+      <div className="flex w-full items-center justify-between px-4 lg:w-[1024px]">
+        <Link href={'/blog'}>
+          <span className="p-1 text-xl font-bold text-black">daehee.dev</span>
+        </Link>
         <MdDarkMode size={24} />
       </div>
     </header>
