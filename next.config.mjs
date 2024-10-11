2d126bcdcffb087d121ea3ed3fe108ea1cef2b04
hdh4952 <gkseogml4959@gmail.com>
2024-10-11 13:17:00 +0000

    feat: '/' -> '/blog'로 리다이렉트

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/absproxy/3000'
+  basePath: '/absproxy/3000',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/blog',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
